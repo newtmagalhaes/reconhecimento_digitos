702a26fddc40f9c1645f78deb5f20ecc5698dfde
anilton <aniltonmagalhaes@gmail.com>
2026-06-11 10:55:22 -0300

    feat: adiciona exploração do dataset
    adiciona numpy, pandas, seaborn e matplotlib.

--- src/config.py
+++ src/config.py
@@ -1,5 +1,24 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).parent.parent
+from sklearn.datasets import fetch_openml
+from sklearn.utils import Bunch
 
+BASE_DIR = Path(__file__).parent.parent
 CACHE_DIR = BASE_DIR / '.cache'
+
+
+def load_mnist() -> Bunch:
+    name = 'mnist_784'
+    df = fetch_openml(
+        name=name,
+        version=1,
+        as_frame=True,
+        data_home=str(CACHE_DIR),
+    )
+
+    return df
+
+
+if __name__ == '__main__':
+    df = load_mnist()
+    print(df)
